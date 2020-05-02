9d168fb10cd0f9174362a9ced1288c322f0810b1
unknown <jani.haantio@gmail.com>
2020-05-02 18:53:12 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import GoogleSignins from './GoogleSignins'
+
+const App = () => {
+  return(
+    <View>
+      <Text> moi</Text>
+      <GoogleSignins></GoogleSignins>
+    </View>
+  )
+}
+
+
+export default App;
---
+++ GoogleSignins.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
+
+export default class GoogleSignins extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text> Google Sig</Text>
+                <GoogleSigninButton
+                    style={{ width: 192, height: 48 }}
+                    size={GoogleSigninButton.Size.Wide}
+                    color={GoogleSigninButton.Color.Dark}
+                    onPress={this._signIn}
+                     />
+            </View>
+        )
+    }
+    componentDidMount(){
+        GoogleSignin.configure({
+            scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+            webClientId: '949429873755-elk9rjcbema7ethhg44ur6p094vpugr4.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+            offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+            //hostedDomain: '', // specifies a hosted domain restriction
+            //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+            forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+            //accountName: '', // [Android] specifies an account name on the device that should be used
+            //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+          });
+    }
+    _signIn=async()=>{
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            console.log(userInfo)
+            //this.setState({ userInfo });
+          } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+              // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+              // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+              // play services not available or outdated
+            } else {
+              // some other error happened
+            }
+            console.log(error)
+          }
+    }
+}
